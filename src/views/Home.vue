8dd5cef66015023e96fb97ef5d1eaddc8e86acb4
jimmy <jonssonjimmy8@gmail.com>
2020-12-15 15:35:33 -0500

    add frontend createPlaces function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
+
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <button v-on:click="createPlaces()">mention a new places</button>
+    <div v-for="place in places">
+      <p><h4>{{place.name}}</h4>
+      <p><h4>{{place.address}}</h4>
+      <h3>{{ message2 }}</h3>
+    <!-- <button v-on:click="indexPlaces()">show the places</button> -->
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      message2: "**************",
+      places: [],
+    };
+  },
+  created: function () {
+    this.indexPlaces();
+  },
+  methods: {
+    indexPlaces: function () {
+      console.log("index of places");
+      axios.get("/api/places").then((response) => {
+        console.log(response.data);
+        this.places = response.data;
+      });
+    },
+    createPlaces: function () {
+      console.log("mention new place");
+      var params = {
+        name: "forest",
+        address: "red tree lane",
+      };
+      axios.post("/api/places", params).then((response) => {
+        console.log(response.data);
+        this.places = response.data;
+      });
+    },
+  },
+};
 </script>
